639e769eae5dc9d93049c1d183f1ff9adf8f6e8e
krgsnv <krigasf@yandex.ru>
2023-05-11 13:24:19 +0300

    add header

---
+++ src/components/App/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Header from '../Header/Header'
+
+import './App.scss'
+
+const App = () => {
+  return (
+    <main className="container">
+        <Header/>
+    </main>
+  )
+}
+
+export default App;
